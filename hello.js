c9db826a67236de2a49bdb9c7545dbe09c4e6fe0
Deanne Chae <deannechae@jeremys-mbp.lan>
2023-08-16 12:49:14 -0500

    now says Hello World

--- hello.js
+++ hello.js
@@ -4,8 +4,8 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello");
-
+console.log("Hello World! It's a nice day!")
+console.log("'Nevermore'.")
 /*  
 Next, PLAY! 
 Modify your code on line 7 to try to accomplish the tasks listed below:
